e8ff58482f4fa4bf3c2075c7c8abc265793636fd
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-11-28 16:19:10 +0100

    label edit button
    label edit button

--- Chinees/Chinees/Form1.cs
+++ Chinees/Chinees/Form1.cs
@@ -148,6 +148,12 @@ namespace Chinees
             mdataReader.Read();
             //display output
             output = mdataReader.GetString(0);
+            Label outputlabel = new System.Windows.Forms.Label();
+            outputlabel.Text = output;
+            Button buttonedit = new System.Windows.Forms.Button();
+            buttonedit.Text = "Aanpassen";
+            Button buttondelete = new System.Windows.Forms.Button();
+            buttondelete.Text = "Verwijderen";
             //db close
             mdataReader.Close();
             cmd.Dispose();
