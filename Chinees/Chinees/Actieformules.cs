a406df1a1b06bc260e1a5cc0884aa0a03433bd29
WiebedeBoer <32057464+WiebedeBoer@users.noreply.github.com>
2019-12-20 15:40:37 +0100

    combobox
    combobox

--- Chinees/Chinees/Actieformules.cs
+++ Chinees/Chinees/Actieformules.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using System.Data;
 
 namespace Chinees
 {
@@ -12,6 +13,12 @@ namespace Chinees
     {
         public SqlConnection conn;
         private int selectedid;
+
+        List<Dictionary<string, int>> list = new List<Dictionary<string, int>>();
+
+        ComboBox comboBox1 = new System.Windows.Forms.ComboBox();
+        ComboBox comboBox2 = new System.Windows.Forms.ComboBox();
+        ComboBox comboBox3 = new System.Windows.Forms.ComboBox();
 
         public Actieformules(int selectedid)
         {            
@@ -69,13 +76,22 @@ namespace Chinees
             return checking;
         }
 
-        //dropdown selecting
-        private List<List<string>> Verhoudingmaker()
+        /*
+        public void add_tolist(int name, Dictionary<string, int> edges)
         {
-            List<List<string>> list = new List<List<string>>();
+            list[name] = edges;
+        }
+        */
+
+        /*
+        //dropdown selecting
+        private List<Dictionary<string, int>> Verhoudingmaker()
+        {
+            
             //search id
             int sid = this.selectedid;
-            string did, output, aid, aoutput, cid, coutput;
+            string output, aoutput, coutput;
+            int did, aid, cid;
 
             //connection
             conn = new DBHandler().getConnection();
@@ -95,16 +111,16 @@ namespace Chinees
             dataReader = cmd.ExecuteReader();
             while (dataReader.Read())
             {
-                did = dataReader.GetString(0);
+                did = Convert.ToInt32(dataReader.GetString(0));
                 output = dataReader.GetString(1);
-                //list.Add(did,output);
+                //list.add_tolist(did, output);
             }
             acmd = new SqlCommand(aquery, conn);
             //acmd.Parameters.Add(new SqlParameter("@sid", sid));
             adataReader = acmd.ExecuteReader();
             while (adataReader.Read())
             {
-                aid = adataReader.GetString(0);
+                aid = Convert.ToInt32(adataReader.GetString(0));
                 aoutput = adataReader.GetString(1);
                 //list.Add(aid,aoutput);
             }
@@ -113,7 +129,7 @@ namespace Chinees
             cdataReader = ccmd.ExecuteReader();
             while (cdataReader.Read())
             {
-                cid = adataReader.GetString(0);
+                cid = Convert.ToInt32(adataReader.GetString(0));
                 coutput = adataReader.GetString(1);
                 //list.Add(aid,aoutput);
             }
@@ -127,31 +143,149 @@ namespace Chinees
             conn.Close();
             return list;
         }
+        */
 
+
+
+        private void Combo_Load()
+        {
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new System.Drawing.Point(700, 40);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(180, 23);
+            //connection
+            conn = new DBHandler().getConnection();
+            SqlCommand sc = new SqlCommand("SELECT ID, Naam FROM Kruidenformules ORDER BY Naam ASC", conn);
+            SqlDataReader reader;
+
+            reader = sc.ExecuteReader();
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("Naam", typeof(string));
+            dt.Load(reader);
+
+            comboBox1.ValueMember = "ID";
+            comboBox1.DisplayMember = "Naam";
+            comboBox1.DataSource = dt;
+            //db close
+            reader.Close();
+            sc.Dispose();
+            conn.Close();
+        }
+
+
+
+
+
+        /*
+    private void ShowDropDown()
+    {
+        //search id
+        string output, aoutput, coutput;
+        int did, aid, cid;
+
+        //connection
+        conn = new DBHandler().getConnection();
+        //command and query strings
+        SqlCommand cmd, acmd, ccmd;
+        String query, aquery, cquery;
+        SqlDataReader dataReader, adataReader, cdataReader;
+        //db open
+        conn.Open();
+        //queries
+        query = "SELECT ID, Naam FROM Kruidenformules ORDER BY Naam ASC";
+        aquery = "SELECT ID, Nederlands FROM Patentformules ORDER BY Nederlands ASC";
+        cquery = "SELECT ID, Syndroom FROM Syndromen ORDER BY Syndroom ASC";
+        //selecting
+        cmd = new SqlCommand(query, conn);
+        //cmd.Parameters.Add(new SqlParameter("@sid", sid));
+        dataReader = cmd.ExecuteReader();
+        while (dataReader.Read())
+        {
+            did = Convert.ToInt32(dataReader.GetString(0));
+            output = dataReader.GetString(1);
+            //list.add_tolist(did, output);
+            //cmdparameter.Items.Add(did,output);
+            cmdparameter.DisplayMember = "ItemName"; //display member which displays on screen
+            cmdparameter.ValueMember = "ItemID"; //ID
+        }
+        acmd = new SqlCommand(aquery, conn);
+        //acmd.Parameters.Add(new SqlParameter("@sid", sid));
+        adataReader = acmd.ExecuteReader();
+        while (adataReader.Read())
+        {
+            aid = Convert.ToInt32(adataReader.GetString(0));
+            aoutput = adataReader.GetString(1);
+            //list.Add(aid,aoutput);
+        }
+        ccmd = new SqlCommand(cquery, conn);
+        //ccmd.Parameters.Add(new SqlParameter("@sid", sid));
+        cdataReader = ccmd.ExecuteReader();
+        while (cdataReader.Read())
+        {
+            cid = Convert.ToInt32(adataReader.GetString(0));
+            coutput = adataReader.GetString(1);
+            //list.Add(aid,aoutput);
+        }
+        //db close
+        dataReader.Close();
+        adataReader.Close();
+        cdataReader.Close();
+        cmd.Dispose();
+        acmd.Dispose();
+        ccmd.Dispose();
+        conn.Close();
+        return list;
+    }
+    */
+
+            /*
         //dropdown display
         public void DropdownMaker()
         {
             List<List<string>> list = new List<List<string>>();
-            ComboBox comboBox1 = new System.Windows.Forms.ComboBox();
+
+            //ComboBox comboBox1 = new System.Windows.Forms.ComboBox();
             comboBox1.FormattingEnabled = true;
+
             //data binding
-            /*
+            //ComboboxItem selectedItem = (ComboboxItem)comboBox1.SelectedItem;
+            //ComboboxItem item1 = new ComboboxItem();
+
+            //item1.Text = "test";
+            //item1.Value = "123";
+
+            
             comboBox1.Items.AddRange(new object[] {
-                foreach (string name in names)
-                {
-                ""
+            "Nederlandse naam kruid",
+            "Latijnse naam kruid",
+            "Thermodynamisch in kruid",
+            "Indicaties in kruidenformule",
+            "Naam kruidenformule",
+            "Kruid in kruidenformule",
+            "Nederlandse naam patentformule",
+            "Engelse naam patentformule",
+            "Pinjin naam patentformule",
+            "Syndroom naam",
+            "Syndroom op symptomen pols en tong",
+            "Patentformule op symptoom"});
+            
 
+            //comboBox1.AddItem "Left Top";
 
-                }
-            });
-            */
+            comboBox1.DisplayMember = "Text";
+            comboBox1.ValueMember = "Value";
+            //List<ComboboxItem> items = new List<ComboboxItem>;
+            //comboBox1.DataSource = items;
+
             comboBox1.Location = new System.Drawing.Point(700, 40);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new System.Drawing.Size(180, 23);
 
-            ComboBox comboBox2 = new System.Windows.Forms.ComboBox();
-            comboBox1.FormattingEnabled = true;
-            /*
+            //ComboBox comboBox2 = new System.Windows.Forms.ComboBox();
+            comboBox2.FormattingEnabled = true;
+            
             comboBox2.Items.AddRange(new object[] {
                 foreach (string name in names)
                 {
@@ -160,14 +294,14 @@ namespace Chinees
 
                 }
             });
-            */
+            
             comboBox2.Location = new System.Drawing.Point(900, 40);
             comboBox2.Name = "comboBox1";
             comboBox2.Size = new System.Drawing.Size(180, 23);
 
-            ComboBox comboBox3 = new System.Windows.Forms.ComboBox();
+            //ComboBox comboBox3 = new System.Windows.Forms.ComboBox();
             comboBox3.FormattingEnabled = true;
-            /*
+            
             comboBox2.Items.AddRange(new object[] {
                 foreach (string name in names)
                 {
@@ -176,10 +310,29 @@ namespace Chinees
 
                 }
             });
-            */
+            
             comboBox3.Location = new System.Drawing.Point(1100, 40);
             comboBox3.Name = "comboBox1";
             comboBox3.Size = new System.Drawing.Size(180, 23);
+
+            //insert button
+            Button buttonin = new System.Windows.Forms.Button();
+            buttonin.Location = new System.Drawing.Point(1160, 40);
+            buttonin.Text = "Koppel";
+            buttonin.Size = new System.Drawing.Size(75, 35);
+            buttonin.Click += new System.EventHandler(this.buttonin_Click);
+            buttonin.Name = "Invoeren";
+
+        }
+    */
+
+        //insert event
+        private void buttonin_Click(object sender, EventArgs e)
+        {
+            int syndroomid = Convert.ToInt32(comboBox1.SelectedValue);
+            int formuleid = Convert.ToInt32(comboBox2.SelectedValue);
+            int patentid = Convert.ToInt32(comboBox3.SelectedValue);
+            Inserter(syndroomid, formuleid, patentid);
         }
 
         //list checking
